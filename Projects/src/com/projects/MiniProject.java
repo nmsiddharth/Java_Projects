22ede17df6ba5332f09fa46a61c245190f85262b
nmsiddharth <siddharthnm07@gmail.com>
2023-12-27 16:52:17 +0530

    Completed Guessing Number

---
+++ Projects/src/com/projects/MiniProject.java
@@ -0,0 +1,53 @@
+package com.projects;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class MiniProject {
+
+	public static int Random() {
+		Random rand = new Random();
+		int rand_Int = rand.nextInt(10)+1;
+		return rand_Int;
+	}
+	
+	public static void check() {
+		Scanner sc= new Scanner(System.in);
+		
+		int count = 0;
+		
+		//boolean flag= false;
+		int temp = 0;
+		int tempRandom = 0;
+		do {
+			
+			System.out.println("Enter the Number");
+			int number = sc.nextInt();
+			temp = number;
+			
+			int random = Random();
+			tempRandom = random;
+
+			System.out.println("Random Number = "+random);
+			if(number<=tempRandom) {
+//				flag = true;
+				count = count +1;
+				System.out.println("YES");
+				
+			}
+		}while(temp<=tempRandom);
+		
+		System.out.println("Final score = "+count);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		
+		
+		MiniProject.check();
+		
+
+	}
+
+}
