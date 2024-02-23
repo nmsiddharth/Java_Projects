aed8cfe45a3944e50ea677d256a754b4c72872d4
nmsiddharth <siddharthnm07@gmail.com>
2024-02-23 11:02:28 +0530

    Completed Employee Management System Project using Hibernate.

--- Employees_Management_System/src/main/java/com/java/Employees_Management_System/EmployeeUtility.java
+++ Employees_Management_System/src/main/java/com/java/Employees_Management_System/EmployeeUtility.java
@@ -1,5 +1,104 @@
 package com.java.Employees_Management_System;
 
-public class EmployeeUtility {
+import java.util.List;
+import java.util.Scanner;
 
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class EmployeeUtility {
+private static SessionFactory factory = null;
+	
+	private static SessionFactory getSessionFactory() {
+		if(factory==null) {
+			factory = new Configuration().configure().buildSessionFactory();
+		}
+		return factory;
+	}
+	
+	  public static void shutdown() {
+	        factory.close();
+	    }
+	
+	public static String addEmployee(Scanner sc) {
+		System.out.println("Enter the Employee ID : ");
+		int empId = sc.nextInt();
+		System.out.println("Enter the Employee Name : ");
+		String empName = sc.next();
+		System.out.println("Enter the Employee Department : ");
+		String empDept = sc.next();
+		
+		Session session = getSessionFactory().openSession();
+		session.beginTransaction();
+		Employee employee = new Employee();
+		 employee.setEmpId(empId);
+         employee.setEmpName(empName);
+         employee.setEmpDept(empDept);
+         session.save(employee);
+         session.getTransaction().commit();
+ 		 return "Employee added successfully";
+	}
+	
+	public static void getAllEmployees() {
+		Session session = getSessionFactory().openSession();
+		Query<Employee> query = session.createQuery("from Employee");
+		List<Employee> employees = query.list();
+		System.out.println("Displaying Employees Details");
+        System.out.println("------------------------------------------");
+        for (Employee employee : employees) {
+            System.out.println("\tEmployeeID   \tEmployeeName   \tDepartmentName");
+            System.out.println("\t" + employee.getEmpId() + "\t" + employee.getEmpName() + "\t" + employee.getEmpDept());
+        }
+	}
+	
+	public static void getEmployeeById(Scanner sc) {
+		Session session = getSessionFactory().openSession();
+		 System.out.println("Enter the ID of Employee");
+         int empId = sc.nextInt();
+         Employee employee = session.get(Employee.class, empId);
+         if (employee != null) {
+             System.out.println("Displaying Employee Details");
+             System.out.println("------------------------------------------");
+             System.out.println("\tEmployeeID   \tEmployeeName   \tDepartmentName");
+             System.out.println("\t" + employee.getEmpId() + "\t" + employee.getEmpName() + "\t" + employee.getEmpDept());
+         } else {
+             System.out.println("Employee with ID " + empId + " not found.");
+         }
+	}
+	
+	public static String updateEmployee(Scanner sc) {
+		Session session = getSessionFactory().openSession();
+		 System.out.println("Enter the ID of Employee");
+         int empId = sc.nextInt();
+         Employee employee = session.get(Employee.class, empId);
+         if (employee != null) {
+             System.out.println("Enter the Department Name");
+             String empDept = sc.next();
+             session.beginTransaction();
+             employee.setEmpDept(empDept);
+             session.update(employee);
+             session.getTransaction().commit();
+             return "Employee updated successfully";
+         } else {
+             return "Employee with ID " + empId + " not found.";
+         }
+	}
+	
+	public static String deleteEmployee(Scanner sc) {
+		Session session = getSessionFactory().openSession();
+		 System.out.println("Enter the ID of Employee");
+         int empId = sc.nextInt();
+         Employee employee = session.get(Employee.class, empId);
+         if (employee != null) {
+             session.beginTransaction();
+             session.delete(employee);
+             session.getTransaction().commit();
+             return "Employee deleted successfully";
+         } else {
+             return "Employee with ID " + empId + " not found.";
+         }
+	}
 }
+
--- Employees_Management_System/src/main/java/com/java/Employees_Management_System/Test.java
+++ Employees_Management_System/src/main/java/com/java/Employees_Management_System/Test.java
@@ -1,10 +1,45 @@
 package com.java.Employees_Management_System;
 
+import java.util.Scanner;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("-------------Employee Management System--------------");
+		char res;
+		do {
+			System.out.println("Select Option");
+			System.out.println("1.addEmployee  2.getAll  3.getById   4.updateEmployee   5.deleteEmployee");
+			int option = sc.nextInt();
+			
+			switch (option) {
+            case 1:
+                System.out.println(EmployeeUtility.addEmployee(sc));
+                break;
+            case 2:
+                EmployeeUtility.getAllEmployees();
+                break;
+            case 3:
+                EmployeeUtility.getEmployeeById(sc);
+                break;
+            case 4:
+                System.out.println(EmployeeUtility.updateEmployee(sc));
+                break;
+            case 5:
+                System.out.println(EmployeeUtility.deleteEmployee(sc));
+                break;
+            default:
+                System.out.println("Invalid Option");
+        }
+        System.out.println("Do you want to continue? y/n");
+        res = sc.next().charAt(0);
+
+    } while (res == 'y');
+    EmployeeUtility.shutdown();
+    System.out.println("Thanks! Have a Good Day");
+}
 
 	}
 
-}
+
